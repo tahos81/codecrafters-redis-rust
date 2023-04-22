677c690489793cfc46897621c0f27ff6a37c83e8
tahos81 <tahirozpala@gmail.com>
2023-04-22 17:56:24 +0300

    Stage 3 Attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,12 +11,30 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write_all(b"+PONG\r\n").unwrap();
+                handle_stream(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    println!("accepted new connection");
+
+    let mut input = [0u8; 512];
+
+    loop {
+        let bytes_read = stream.read(&mut input).unwrap_or_default();
+
+        if bytes_read == 0 {
+            break;
+        } else {
+            println!("bytes read: {}", bytes_read);
+            println!("{}", std::str::from_utf8(&input).unwrap());
+            stream.write_all(b"+PONG\r\n").unwrap();
+            input = [0u8; 512];
         }
     }
 }
